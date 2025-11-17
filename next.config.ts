04f105f0353999ffb8c308c11998e40b0fe69917
Jake Palmer <jpalmer@restorationstl.com>
2025-11-17 06:30:51 -0600

    fix: webpack externals for Payload CMS on Vercel

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,19 @@ import { withPayload } from '@payloadcms/next/withPayload'
 const nextConfig = {
   // Your Next.js config here
   reactCompiler: true,
+  
+  // Webpack config to handle Payload CMS dependencies
+  webpack: (config: any, { isServer }: { isServer: boolean }) => {
+    if (isServer) {
+      config.externals = config.externals || []
+      config.externals.push({
+        'thread-stream': 'commonjs thread-stream',
+        'pino': 'commonjs pino',
+        'pino-pretty': 'commonjs pino-pretty',
+      })
+    }
+    return config
+  },
 }
 
 // Make sure you wrap your `nextConfig`
